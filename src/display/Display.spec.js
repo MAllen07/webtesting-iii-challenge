3ce6fc7ce405af922467751c43f8768067e3b076
MAllen07 <mrsmallen07@gmail.com>
2019-06-19 17:42:25 -0400

    Added new dependencies,  created tests in dashboards - snapshot created - confirmed working

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,28 @@
 // Test away!
+
+describe.skip("display component", () => {
+    it.todo("displays if gate is open/closed and if it is locked/unlocked"),
+        () => {
+            //write assurtion in this block
+        };
+    it.todo(
+        "displays 'Closed' if the closed prop is true and 'Open' if otherwise"
+    ),
+        () => {
+            //write assurtion in this block
+        };
+    it.todo(
+        "displays 'Locked' if the locked prop is true and 'Unlocked' if otherwise"
+    ),
+        () => {
+            //write assurtion in this block
+        };
+    it.todo("when locked or closed use the red-led class"),
+        () => {
+            //write assurtion in this block
+        };
+    it.todo("when unlocked or open use the green-led class"),
+        () => {
+            //write assurtion in this block
+        };
+});
